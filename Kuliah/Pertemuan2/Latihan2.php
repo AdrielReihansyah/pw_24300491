4c41b4b0a35cb13cf286d32ff8c2ba340466a336
AdrielReihansyah <adrielreihansyah05@gmail.com>
2025-02-17 22:56:17 +0700

    first commit

---
+++ Kuliah/Pertemuan2/Latihan2.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nama = "Adriel Reihansyah";
+    $email = "adrielreihansyah05@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Universitas Pasundan";
+
+    echo "<h1>Halo! Nama Saya $nama</h1>";
+    echo "<p>alamat email saya : $email</p>";
+    echo "<p>pekerjaan saya saat ini sebagai : $pekerjaan</p>";
+    echo "<p>jurusan saya dikampus adalah : $jurusan</p>";
+    echo "<p>saya berkuliah di : $universitas</p>";
+
+?>
---
+++ Tugas/1a.php
@@ -0,0 +1,5 @@
+<?php
+    $cap = "Topi";
+    $circle = "Bundar";
+    echo "<h2><i>\"$cap saya $circle, $circle $cap saya.\"</i></h2>";
+?>
